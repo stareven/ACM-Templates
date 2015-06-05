f365776b86555dcf09c289e344b551fefebb6df1
stareven <neverats@gmail.com>
2015-06-05 17:14:24 +0800

    add ds/FenwickTree

---
+++ ds/FenwickTree/FenwickTree.cpp
@@ -0,0 +1,46 @@
+#include "FenwickTree.h"
+#include <assert.h>
+
+namespace ds {
+
+using namespace std;
+
+FenwickTree::FenwickTree(size_t upper)
+{
+    assert(upper > 0);
+    while (upper > lowbit(upper)) upper += lowbit(upper);
+    _tree.resize(upper + 1);
+}
+
+FenwickTree::~FenwickTree()
+{
+}
+
+void FenwickTree::update(size_t x, int inc)
+{
+    assert(x < _tree.size());
+    for (size_t i = x; i < _tree.size(); i += lowbit(i)) _tree[i] += inc;
+}
+
+int FenwickTree::query(size_t x)
+{
+    assert(x < _tree.size());
+    int sum = 0;
+    for (size_t i = x; i > 0; i -= lowbit(i)) sum += _tree[i];
+    return sum;
+}
+
+size_t FenwickTree::bsearch(int x)
+{
+    assert(_tree.back() >= x);
+    size_t cnt = 0;
+    int sum = 0;
+    for (size_t i = _tree.size() - 1; i > 0; i >>= 1) {
+        if (sum + _tree[cnt + i] >= x) continue;
+        sum += _tree[cnt += i];
+    }
+    return cnt + 1;
+}
+
+
+}
---
+++ ds/FenwickTree/FenwickTree.h
@@ -0,0 +1,30 @@
+#ifndef _DS_FENWICK_TREE_H_
+#define _DS_FENWICK_TREE_H_
+
+#include <vector>
+#include <stdlib.h>
+
+namespace ds {
+
+class FenwickTree
+{
+public:
+    explicit FenwickTree(size_t upper);
+    ~FenwickTree();
+
+public:
+    void update(size_t x, int inc);
+    int query(size_t x);
+    size_t bsearch(int x);
+
+private:
+    static size_t lowbit(size_t x) { return -x & x; }
+
+private:
+    std::vector<int> _tree;
+
+};
+
+}
+
+#endif // _DS_FENWICK_TREE_H_
